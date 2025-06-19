720eaebb6c5aaa791d63d0c4479237180d34218e
Klopov <klopov.1995@list.ru>
2025-06-20 02:09:45 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,11 @@ import io.javalin.Javalin;
 
 public class App {
 
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
     public static Javalin getApp() {
         // Настройка приложения
         var app = Javalin.create(config -> {
@@ -16,6 +21,8 @@ public class App {
     }
 
     public static void main(String[] args) {
-        getApp().start(7070);
+        getApp().start(getPort());
     }
+
+
 }
